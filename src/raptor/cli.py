b03838750ca5fa3d8ce737b2e8b289416365e0b9
John <colemanjs@ornl.gov>
2025-07-09 09:27:42 -0400

    initial cli clean up

--- src/raptor/cli.py
+++ src/raptor/cli.py
@@ -19,7 +19,7 @@ from .structures import MeltPool
 
 def main() -> int:
     """
-    Parses args, loads config, runs porosity prediction. Returns exit code.
+    Parses args, loads config, runs porosity prediction, processes pore data.
     """
     parser = argparse.ArgumentParser(description="LPBF Porosity Predictor")
     parser.add_argument("config_file", type=str, help="Path to YAML config file.")
@@ -30,34 +30,35 @@ def main() -> int:
             print(f"Error: Config file not found: {args.config_file}")
             return 1
         with open(args.config_file, "r") as f:
-            cfg = yaml.safe_load(f)
-        cfg_dir = os.path.dirname(os.path.abspath(args.config_file))
+            config = yaml.safe_load(f)
+        config_dir = os.path.dirname(os.path.abspath(args.config_file))
         print(f"Loaded config: {args.config_file}")
     except Exception as e:
         print(f"Error loading/parsing YAML '{args.config_file}': {e}")
         return 1
 
     try:
-        sfp_rel = cfg["scan_paths"]
-        vtk_rel = cfg["output_vtk_filename"]
-        morph_rel = cfg["output_morphology_filename"]
-        morph_fields = cfg["output_morphology_fields"]
-        p = cfg.get("parameters", {})
-        lh_val = p["layer_height"]
-        n_pts_bh = int(p["bezier_points_per_half"])
-        d_res = p["voxel_resolution"]
-        en_rand_ph = p["enable_random_segment_phase"]
+        # read scan path files
+        scan_paths = config.get("scan_paths", {})
 
-        mp = cfg["melt_pool_data"]
-        mp_full_data = {}
-        for key in mp:
-            datatype = mp[key]["type"]
+        # read simulation parameter dictionary
+        parameters = config.get("parameters", {})
+        layer_height = parameters["layer_height"]
+        n_bezier_pts_half = int(parameters["bezier_points_per_half"])
+        voxel_resolution = parameters["voxel_resolution"]
+        en_rand_ph = parameters["enable_random_segment_phase"]
+
+        # read melt pool dictionary (time series or spectral components)
+        melt_pool_dict = config.get("melt_pool_data", {})
+        melt_pool_data = {}
+        for key in melt_pool_dict:
+            datatype = melt_pool_dict[key]["type"]
             if datatype == "time_series":
                 try:
-                    filepath = mp[key]["file_name"]
-                    nmodes = int(mp[key]["nmodes"])
-                    scale = mp[key]["scale"]
-                    mp_full_data[key] = (read_data(filepath), scale, nmodes)
+                    filepath = melt_pool_dict[key]["file_name"]
+                    nmodes = int(melt_pool_dict[key]["nmodes"])
+                    scale = melt_pool_dict[key]["scale"]
+                    melt_pool_data[key] = (read_data(filepath), scale, nmodes)
                 except:
                     print(
                         "Error reading the specified {} {} data format.".format(
@@ -66,12 +67,12 @@ def main() -> int:
                     )
             elif datatype == "spectral_components":
                 try:
-                    filepath = mp[key]["file_name"]
-                    nmodes = int(mp[key]["nmodes"])
-                    scale = mp[key]["scale"]
+                    filepath = melt_pool_dict[key]["file_name"]
+                    nmodes = int(melt_pool_dict[key]["nmodes"])
+                    scale = melt_pool_dict[key]["scale"]
                     print("Scaling the zeroth mode (mean) of {} spectral array.")
                     spec_array = read_data(filepath)
-                    mp_full_data[key] = (spec_array, scale, nmodes)
+                    melt_pool_data[key] = (spec_array, scale, nmodes)
                 except:
                     print(
                         "Error reading the specified {} {} data format.".format(
@@ -79,61 +80,56 @@ def main() -> int:
                         )
                     )
 
-        rve = cfg.get("rve", {})
+        # read representative volume element (RVE) dictionary
+        rve = config.get("rve", {})
         try:
-            p0 = rve["min_point"]
-            p1 = rve["max_point"]
-            boundingBox = np.array([p0, p1])
+            bounding_box = np.array([rve["min_point"], rve["max_point"]])
         except:
-            print("Warning: 'boundBox' was not found, defaulting to none")
-            boundingBox = None
-        """
-        rve = cfg["rve"]
-        if rve["boundBox"] is None:
-            boundingBox = None
-        else:
-            p0 = rve["boundBox"]["p0"]
-            p1 = rve["boundBox"]["p1"]
-            boundingBox = np.array([p0,p1])
-        """
+            print("Warning: 'rve' was not found, defaulting to none")
+            bounding_box = None
+
+        # read output dictionary
+        output = config.get("output", {})
+        vtk_dict = output.get("vtk", {})
+        morphology_dict = output.get("morphology", {})
+        vtk_file_name = vtk_dict.get("file_name", None)
+        morphology_file_name = morphology_dict.get("file_name", None)
+        morphology_fields = morphology_dict.get("fields", None)
+
+        print(vtk_file_name, morphology_file_name, morphology_fields)
+
     except KeyError as e:
         print(f"Error: Missing key '{e}' in YAML config '{args.config_file}'.")
         return 1
 
-    if not isinstance(sfp_rel, list) or not sfp_rel:
-        print("Error: 'scan_paths' must be a non-empty list.")
-        return 1
-    if not isinstance(vtk_rel, str) or not vtk_rel:
-        print("Error: 'output_vtk_filename' must be a string.")
-        return 1
-    if not isinstance(morph_rel, str) or not morph_rel:
-        print("Error: 'output_morphology_filename' must be a string.")
-        return 1
-    if not isinstance(lh_val, (int, float)) or lh_val <= 0:
-        print("Error: 'layer_height' must be a positive number.")
-        return 1
-    if not isinstance(n_pts_bh, int) or n_pts_bh < 2:
-        print("Error: 'bezier_points_per_half' must be int >= 2.")
-        return 1
-
-    sfp_abs = [
-        os.path.join(cfg_dir, pth) if not os.path.isabs(pth) else pth for pth in sfp_rel
+    # convert relative file paths to absolute file paths
+    scan_path_files = [
+        os.path.join(config_dir, path) if not os.path.isabs(path) else path
+        for path in scan_paths
     ]
-    vtk_abs = os.path.join(cfg_dir, vtk_rel) if not os.path.isabs(vtk_rel) else vtk_rel
-    morph_abs = (
-        os.path.join(cfg_dir, morph_rel) if not os.path.isabs(morph_rel) else morph_rel
+    vtk_file = (
+        os.path.join(config_dir, vtk_file_name)
+        if not os.path.isabs(vtk_file_name)
+        else vtk_file_name
+    )
+    morpholopy_file = (
+        os.path.join(config_dir, morphology_file_name)
+        if not os.path.isabs(morphology_file_name)
+        else morphology_file_name
     )
 
+    # report summary of input parameters for simulation
     print("\n--- Simulation Parameters ---")
     param_summary = {
-        "Scan Paths": sfp_abs,
+        "Scan Paths": scan_path_files,
         "  Random Phases": f"{en_rand_ph}",
-        "Layer Height": f"{lh_val:.2e} m",
-        "VTK Output": vtk_abs,
-        "Morphology Output": morph_abs,
-        "Voxel Res": f"{d_res:.2e} m",
-        "Bezier Pts/Half": n_pts_bh,
+        "Layer Height": f"{layer_height:.2e} m",
+        "VTK Output": vtk_file,
+        "Morphology Output": morpholopy_file,
+        "Voxel Res": f"{voxel_resolution:.2e} m",
+        "Bezier Pts/Half": n_bezier_pts_half,
     }
+
     for k, v_item in param_summary.items():
         if isinstance(v_item, list):
             print(f"  {k}:")
@@ -142,41 +138,63 @@ def main() -> int:
             print(f"  {k}: {v_item}")
 
     print("  Melt pool data:")
-    for key in mp_full_data:
-        print(f"    {key} datatype: " + mp[key]["type"])
-        print(f"    {key} path : " + mp[key]["file_name"])
-        print(f"    {key} scaling : {mp_full_data[key][1]}")
-        print(f"    {key} modes : {mp_full_data[key][2]}")
+    for key in melt_pool_data:
+        print(f"    {key} datatype: " + melt_pool_dict[key]["type"])
+        print(f"    {key} path : " + melt_pool_dict[key]["file_name"])
+        print(f"    {key} scaling : {melt_pool_data[key][1]}")
+        print(f"    {key} modes : {melt_pool_data[key][2]}")
 
-    if boundingBox is not None:
+    if bounding_box is not None:
         print("  RVE data:")
-        print(f"    xmin,ymin,zmin = {p0[0]} m,{p0[1]} m,{p0[2]} m")
-        print(f"    xmax,ymax,zmax = {p1[0]} m,{p1[1]} m,{p1[2]} m")
+        print(
+            f"    xmin,ymin,zmin = {bounding_box[0][0]} m,{bounding_box[0][1]} m,{bounding_box[0][2]} m"
+        )
+        print(
+            f"    xmax,ymax,zmax = {bounding_box[1][0]} m,{bounding_box[1][1]} m,{bounding_box[1][2]} m"
+        )
 
     try:
 
-        mp = construct_meltpool(mp_full_data, en_rand_ph)
+        # construct melt pools
+        melt_pool = construct_meltpool(melt_pool_data, en_rand_ph)
 
-        origin, porosity = compute_porosity(
-            sfp_abs, lh_val, d_res, n_pts_bh, mp, boundBox=boundingBox
+        # compute porosity
+        centroid, porosity = compute_porosity(
+            scan_path_files,
+            layer_height,
+            voxel_resolution,
+            n_bezier_pts_half,
+            melt_pool,
+            bounding_box,
         )
 
-        write_vtk(origin, d_res, porosity, vtk_abs)
-        if morph_fields:
-            props = compute_morphology(porosity, d_res, morph_fields)
-            write_morphology(props, morph_abs)
+        # write VTK (optional)
+        if vtk_dict:
+            write_vtk(centroid, voxel_resolution, porosity, vtk_file)
+
+        # write morphology metrics (optional)
+        if morphology_fields:
+            write_morphology
+            (
+                compute_morphology(porosity, voxel_resolution, morphology_fields),
+                morpholopy_file,
+            )
+
     except FileNotFoundError as e:
         print(f"Error: {e}")
         return 1
+
     except ValueError as ve:
         print(f"Error: {ve}")
         return 1
+
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
         import traceback
 
         traceback.print_exc()
         return 1
+
     return 0
 
 
