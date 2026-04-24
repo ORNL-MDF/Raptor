b2b522e9cef485c3e49c7aa119b1f7961f9a1d29
Konstantin Pieper <pieperk@ornl.gov>
2026-04-24 13:21:22 -0400

    add sable backend, nonlinear output normalization, and statistics
    - add sable backend as alternative to sklearn
    - add nonlinear output normalization to scale errors to log scale for
      acquisition function
    - tweak and improve the way statistics are computed

--- examples/active_learning_example/defect_model/create_defect_model.py
+++ examples/active_learning_example/defect_model/create_defect_model.py
@@ -64,8 +64,12 @@ MAX_ITERATIONS = 30
 
 SEED = 42
 
-VOXEL_RESOLUTION_M = 2e-6  # increase voxel_resolution to speed up
+VOXEL_RESOLUTION_M = 5e-6  # increase voxel_resolution to speed up
 RVE_LENGTH_M       = 5e-4
+
+MIN_LEN_DEFECTS    = 10
+ANALYZE_MAX        = False
+BACKEND            = "sklearn" #"sable"
 
 @njit
 def seed_numba(seed):
@@ -118,16 +122,22 @@ def run_raptor(
     hatch_spacing_m: float,
     mp_interpolator: MeltPoolInterpolator,
     layer_thickness_m: float = 40e-6,
-    query_volume_mm3: float = 1.0,  # is not used curently?
+    query_volume_mm3: float = 1.0,  # is not used currently?
     voxel_resolution_m: float = 5e-6,
     metric_names: list[str] = ["equivalent_diameter_area"],
 ):
-    random.seed(SEED)
-    np.random.seed(SEED)
-    seed_numba(SEED)
+    # query volume: should be set to the value from size of the experimental coupon
+    #    (artifact for process quality control)
+
+    # TODO: why do we do the seeding in every iteration?
+    # this is causing issues with the statistics, I commented it out.
+    #random.seed(SEED)
+    #np.random.seed(SEED)
+    #seed_numba(SEED)
 
     mp_stats = mp_interpolator.query(LASER_VELOCITY_M_S, LASER_POWER_WATTS)
 
+    # TODO: run enough RVSs to cover the whole query volume.
     rve_min_point = np.array([0.0, 0.0, 0.0])
     rve_max_point = np.array([RVE_LENGTH_M, RVE_LENGTH_M, RVE_LENGTH_M])
     rve_bounding_box = np.array([rve_min_point, rve_max_point])
@@ -187,47 +197,82 @@ def process_raptor_data(raptor_data):
     voxel_resolution_m = raptor_data["inputs"]["voxel_resolution_m"]
     combined_defects = raptor_data["outputs"]["equivalent_diameter_area"]
 
-    # add 3 pores below the voxel_resolution, to deal with empty lists due to finite resolution
-    # TODO: discuss this, if we want to take a maximum here, we need to think about the extreme value statistics
-    combined_defects = combined_defects.tolist() + (voxel_resolution_m * np.random.rand(3)).tolist()
+    min_len_defects = MIN_LEN_DEFECTS
+    if len(combined_defects) < min_len_defects:
+        # add 3 pores below the voxel_resolution, to deal with empty lists due to finite resolution
+        # TODO: discuss how we should handle the fact that pores blow voxel_resolution can not be resolved
+        random.seed() # explicitly call rng seeding to make sure this is truly random
+        n_extra_defects = min_len_defects - len(combined_defects)
+        more_defects = (voxel_resolution_m * np.random.rand(n_extra_defects)).tolist()
+        combined_defects = combined_defects.tolist() + more_defects
 
-    if len(combined_defects) > 1:
-        max_pore = np.max(combined_defects)
-        std_pore = np.std(combined_defects, ddof=1) / np.sqrt(len(combined_defects))
+    max_pore = np.max(combined_defects)
+    mean_pore = np.mean(combined_defects)
+    std_pore = np.std(combined_defects, ddof=1)
+    # compute the standard error, the standard deviation of the mean (Monte-Carlo error)
+    std_err_pore = std_pore / np.sqrt(len(combined_defects))
+
+    logger.info(f"Found {len(combined_defects)} defects: "
+                f"Hatch: {raptor_data['inputs']['hatch_spacing_m']*1e6:.1f}um | Max Pore: {max_pore*1e6:.2f}um")
+
+    # TODO: discuss and refine the data analyis and extreme value statistics
+    return_mean = not ANALYZE_MAX
+    if return_mean:
+        y, yerr = float(mean_pore), float(std_err_pore)
     else:
-         # if no pores are fouund, they must be below VOXEL_RESOLUTION
-        max_pore = np.max(combined_defects, initial=voxel_resolution_m)
-        std_pore = max_pore
+        # return the maximum pore size, use the standard deveiation as approximate error estimate
+        y, yerr = float(max_pore), float(std_pore)
 
-    logger.info(f"Hatch: {raptor_data['inputs']['hatch_spacing_m']*1e6:.1f}um | Max Pore: {max_pore*1e6:.2f}um")
-
-    return float(max_pore), float(std_pore)
+    return y, yerr
 
 
 # -----------------------------------------------------------------------------
 # UTILITIES
 # -----------------------------------------------------------------------------
-def x_to_unit(X):
-    X = np.asarray(X, dtype=float)
+def x_to_unit(x):
+    x = np.asarray(x, dtype=float)
     lo = np.array([b[0] for b in BOUNDS])
     hi = np.array([b[1] for b in BOUNDS])
-    return (X - lo) / (hi - lo + 1e-12)
+    return (x - lo) / (hi - lo + 1e-12)
 
 
-def x_from_unit(U):
-    U = np.asarray(U, dtype=float)
+def x_from_unit(x):
+    x = np.asarray(x, dtype=float)
     lo = np.array([b[0] for b in BOUNDS])
     hi = np.array([b[1] for b in BOUNDS])
-    return U * (hi - lo) + lo
+    return x * (hi - lo) + lo
 
 
 def get_data_point(x_suggested, mp_interpolator):
     x_ = [np.clip(x_suggested[0], BOUNDS[0][0], BOUNDS[0][1])]
-    raptor_data = run_raptor(x_[0], mp_interpolator,
+    raptor_data = run_raptor(x_[0],
+                             mp_interpolator,
                              voxel_resolution_m=VOXEL_RESOLUTION_M,
                              )
     y_, yerr_ = process_raptor_data(raptor_data)
     return x_, y_, yerr_, raptor_data
+
+
+def y_to_unit(y, yerr, y_scale=(1., 1.)):
+    y_prescale, y_postscale = y_scale
+    # transformation to normalized output for training
+    #   y -> log(1 + y/y_prescale) / y_postscale
+    # scale the errors according to the derivative of the transformation
+    y = np.asarray(y, dtype=float)
+    yerr = np.asarray(yerr, dtype=float)
+    y_norm = np.log1p(y / y_prescale) / y_postscale
+    yerr_norm = yerr / (y + y_prescale) / y_postscale
+    return y_norm.tolist(), yerr_norm.tolist()
+
+
+def y_from_unit(y_norm, yerr_norm, y_scale=(1., 1.)):
+    y_prescale, y_postscale = y_scale
+    y_norm = np.asarray(y_norm, dtype=float)
+    yerr_norm = np.asarray(yerr_norm, dtype=float)
+    y = y_prescale * np.expm1(y_postscale * y_norm)
+    yerr = yerr_norm * (y + y_prescale) * y_postscale
+    return y.tolist(), yerr.tolist()
+
 
 
 # -----------------------------------------------------------------------------
@@ -260,8 +305,12 @@ class ActiveLearningOrchestrator:
         ]
         self.dataset_y, self.dataset_yerr = [list(tuple) for tuple in zip(*dataset_statistics)]
 
-        self.y_mean = np.mean(self.dataset_y)
-        self.y_std = np.std(self.dataset_y, ddof=1) if np.std(self.dataset_y) > 0 else 1.0
+        # scaling factor for output data transformation, pre-scaling, and post-scaling after log transform
+        # crucially, scling the outputs also scales the error bar, which influences the acquisition strategy
+        pre_to_post_scale_ratio = 0.05
+        y_prescale = pre_to_post_scale_ratio * np.max(self.dataset_y)
+        y_postscale = np.log1p(1/pre_to_post_scale_ratio)
+        self.y_scale = (y_prescale, y_postscale)
 
         self.dataset_x_unit = x_to_unit(self.dataset_x).tolist()
         self.bounds_unit = UNIT_BOUNDS
@@ -272,56 +321,86 @@ class ActiveLearningOrchestrator:
         payload = None
         if operation == "initialize_workflow":
 
-            # nondimensionalized lengthscale, on the normalized x data
-            length_scale = .2
-            # prior variance of the kernel
+            # normalize and transform the output data
+            y_norm, yerr_norm = y_to_unit(self.dataset_y, self.dataset_yerr, self.y_scale)
+
+            # prior variance of the kernel (how large is uncertainty without data)
             prior_variance = 2.0
-            # standard deviation of output error yerr
-            yerr = np.asarray(self.dataset_yerr)
 
-            self.heteroscedastic = np.asarray(yerr).shape[0] > 1
-
-            if not self.heteroscedastic:
-                kernel_args = {"length_scale": length_scale, "length_scale_bounds": "fixed",
-                               "constant_value": prior_variance, "constant_value_bounds": "fixed",
-                               "noise_level": yerr**2, "noise_level_bounds": "fixed", # noise level is noise variance
-                               }
-                backend_args = {"alpha": .0}
-            else:
-                kernel_args = {"length_scale": length_scale, "length_scale_bounds": "fixed",
-                               "constant_value": prior_variance, "constant_value_bounds": "fixed",
-                               "noise_level": 0., "noise_level_bounds": "fixed", # set nugget to zero, and use alpha
-                               }
+            self.backend = BACKEND
+            if self.backend == "sklearn":
+                self.kernel = "matern"
+                # nondimensionalized GP lengthscale, on the normalized x data
+                length_scale = .2
+                self.kernel_args = {
+                    "length_scale": length_scale, "length_scale_bounds": "fixed",
+                    "constant_value": prior_variance, "constant_value_bounds": "fixed",
+                    # set "noise_level" (nugget) to zero, and use alpha below for heteroscedastic noise
+                    "noise_level": 0., "noise_level_bounds": "fixed",
+                }
                 # use the nondimensionalized yerr to set alpha
-                backend_args = {"alpha": (yerr/self.y_std)**2}
+                y_variance = np.asarray(yerr_norm)**2
+                self.backend_args = {"alpha": y_variance}
 
-            # normalize the output data
-            y_norm = ((np.array(self.dataset_y) - self.y_mean) / self.y_std).tolist()
+            elif self.backend == "sable":
+                self.kernel = "rbf"
+                self.kernel_args = {
+                    # x range of the data
+                    "x_range": self.bounds_unit[0],
+                    # sigma range of valid lengthscales
+                    "sigma_range": [1e-3, 0.5],
+                    # smoothness hyperparameter gamma
+                    # (0. means the minimum degree of smoothness, i.e. continuous;
+                    #  1. is once differentiable, etc. )
+                    "gamma": 0.1
+                }
+                self.backend_args = {
+                    # memory size for number of features:
+                    # needs to be large enough, but becomes slower with more features
+                    "n_features": 10000,
+                     # prior variance (scaled by problem specific hyperparameter)
+                    "alpha": 0.05/prior_variance,
+                    # algorithm hyperparameters
+                    # p is degree of adaptivity (p=2 is a GP, p=1 is fully sparse)
+                    "p": 1.25,
+                    # number of optimization steps (needs to be large enough, but slows performance)
+                    "n_iter_irls": 100,
+                    # noise level of the data, standard deviation of each data point
+                    "noise_level": yerr_norm,
+                }
 
             payload = DialWorkflowCreationParamsClient(
                 dataset_x=self.dataset_x_unit,
                 dataset_y=y_norm,
                 bounds=self.bounds_unit,
-                kernel="matern",
+                kernel=self.kernel,
                 length_per_dimension=False,
                 y_is_good=False,
-                backend="sklearn",
-                kernel_args=kernel_args,
-                backend_args=backend_args,
+                backend=self.backend,
+                kernel_args=self.kernel_args,
+                backend_args=self.backend_args,
                 seed=SEED,
                 preprocess_standardize=False,
             )
         elif operation == "update_workflow_with_data":
-            next_y_norm = (kwargs["next_y"] - self.y_mean) / self.y_std
+            # normalize / transform the output data
+            y_norm, yerr_norm = y_to_unit(self.dataset_y, self.dataset_yerr, self.y_scale)
+            # just pop the last element of the full dataset (TODO: this interface for updating needs to change)
+            next_y_norm = y_norm[-1]
 
-            if self.heteroscedastic:
-                yerr = np.asarray(self.dataset_yerr)
+            if self.backend == "sklearn":
                 # use the nondimensionalized yerr to set alpha
-                kwargs["backend_args"] = {"alpha": (yerr/self.y_std)**2}
+                y_variance = np.asarray(yerr_norm)**2
+                new_backend_args = {"alpha": y_variance}
+            elif self.backend == "sable":
+                new_backend_args = {"noise_level": yerr_norm}
+            # update backend args with new noise level
+            self.backend_args |= new_backend_args
 
             kwargs["next_y"] = float(next_y_norm)
             payload = DialWorkflowDatasetUpdate(
                 workflow_id=self.workflow_id,
+                backend_args=self.backend_args,
                 **kwargs,
             )
         elif operation == "get_next_point":
@@ -372,16 +451,18 @@ class ActiveLearningOrchestrator:
         if operation == "dial.get_surrogate_values":
             data = payload["data"]
 
-            mean_norm = np.array(data[0])
-            var_norm = np.array(data[1])
+            y_norm_grid = np.array(data[0])
+            yerr_norm_grid = np.array(data[1])
 
-            self.mean_grid = (mean_norm * self.y_std) + self.y_mean
-            self.variance = var_norm**2 * (self.y_std**2)
+            # rescale / transform data back to original units for saving
+            y_grid, yerr_grid = y_from_unit(y_norm_grid, yerr_norm_grid, self.y_scale)
+            self.mean_grid = np.asarray(y_grid)
+            self.variance_grid = np.asarray(yerr_grid)**2
 
             np.savez(
                 "defect_model_surrogate.npz",
                 mean_grid=self.mean_grid,
-                variance_grid=self.variance,
+                variance_grid=self.variance_grid,
                 dataset_x=self.dataset_x,
                 dataset_y=self.dataset_y,
                 bounds=BOUNDS,
